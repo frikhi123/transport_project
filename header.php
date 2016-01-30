c7984bdf5b143f54f816c4cffbc0fe51f26e7b7c
Amine Elouattar <elouattar@gmail.com>
2016-01-30 19:38:06 +0000

    manque de root_url
    manque de root_url f lien dial jQuery

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
       <link rel="stylesheet" href="<?php echo ROOT_URL;?>/assets/css/neon-forms.css" id="style-resource-7">
       <link rel="stylesheet" href="<?php echo ROOT_URL;?>/assets/css/custom.css" id="style-resource-8">
       <link rel="stylesheet" href="<?php echo ROOT_URL;?>/assets/css/skins/facebook.css" id="style-resource-9">
-      <script src="assets/js/jquery-1.11.3.min.js"></script> <!--[if lt IE 9]><script src="http://demo.neontheme.com/assets/js/ie8-responsive-file-warning.js"></script><![endif]--> <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries --> <!--[if lt IE 9]> <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script> <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script> <![endif]--> <!-- TS1453820116: Neon - Responsive Admin Template created by Laborator --> 
+      <script src="<?php echo ROOT_URL;?>/assets/js/jquery-1.11.3.min.js"></script> <!--[if lt IE 9]><script src="http://demo.neontheme.com/assets/js/ie8-responsive-file-warning.js"></script><![endif]--> <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries --> <!--[if lt IE 9]> <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script> <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script> <![endif]--> <!-- TS1453820116: Neon - Responsive Admin Template created by Laborator --> 
    </head>
    <body class="page-body skin-facebook" data-url="http://demo.neontheme.com">
       <!-- TS145382011619125: Xenon - Boostrap Admin Template created by Laborator / Please buy this theme and support the updates --> 
