41f4d05063a06af5c5609b180ccbfa45172b2bf4
Amine Elouattar <elouattar@gmail.com>
2016-01-31 09:33:20 +0000

    ter9a3
    whir ter9a3 w sari, ajax maarftch 3lach makikdemch li, kehl had
    solution tanchuf mal code lakhor

--- admin/show_map.php
+++ admin/show_map.php
@@ -1,39 +1,69 @@
-    <?php 
+<?php 
 	include '../includes/header.php';
 	include '../includes/Menu.php';
 	include '../includes/Navigation.php';
 
-	?>    
+  $db = new PDO('mysql:host=localhost;dbname=transport', 'root', '');
+  $query = $db->prepare('SELECT * FROM students LIMIT 0, 20');
+  $query->execute();
 
- <div class="row">
-   <div class="col-sm-1"></div>
-    <div class="col-sm-10">
-      <div class="panel panel-primary" id="charts_env">
-         <div class="panel-heading">
-            <div class="panel-title">Map</div>
-         </div>
-         <div class="panel-body">
-            <div class="tab-content">
-                <div id="googleMap" style="height:450px;"></div>
-            </div>
-         </div>
-         
-      </div>
-    </div>
+  $nbr = $query->rowCount();
+  echo '<input type = "hidden" value = "'.$nbr.'" id = "nbr">';
+  $i = 1;
+  while($row=$query->fetch()){
+    echo '<input type = "hidden" value = "'.$row['lat'].','.$row['lng'].'" id = "marker'.$i.'">';
+    $i++;
+  }
+?>    
+
+<div class="row">
+<div class="col-sm-1"></div>
+<div class="col-sm-10">
+  <div class="panel panel-primary" id="charts_env">
+     <div class="panel-heading">
+        <div class="panel-title">Map</div>
+     </div>
+     <div class="panel-body">
+        <div class="tab-content">
+            <div id="googleMap" style="height:450px;"></div>
+        </div>
+     </div>
+     
+  </div>
+</div>
 <div id="googleMap"></div>
+</div>
 
- </div>
- <script src="http://maps.googleapis.com/maps/api/js">
-</script>
- <script>
+<script src="http://maps.googleapis.com/maps/api/js"></script>
+<script>
   function initialize() {
     var mapProp = {
       center:new google.maps.LatLng(34.03128,-6.77056),
       zoom:5,
       mapTypeId:google.maps.MapTypeId.ROADMAP
     };
-    var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    var carte = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+    var nbr = document.getElementById('nbr');
+    for(var i = 1; i <= nbr.value; i++){
+      position = document.getElementById('marker'+i).value;
+      tPosition = position.split(',');
+      var loc = new google.maps.LatLng(tPosition[0], tPosition[1]);
+      var marker = new google.maps.Marker({
+        position : loc,
+        map : carte
+
+      });
+    }
+
+    var loc = new google.maps.LatLng(34.259197, -6.588783);
+    var marker = new google.maps.Marker({
+      position : loc,
+      map : carte
+
+    });
   }
+
   google.maps.event.addDomListener(window, 'load', initialize);
 </script>
 <?php 	include '../includes/footer.php';	?>  
